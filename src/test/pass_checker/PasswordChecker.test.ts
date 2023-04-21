dbf336b10d22cca7e8ec2ef60a1e1a211c028635
shitakemura <shitakemura@gmail.com>
2023-04-21 16:57:47 +0900

    first test iteration for password checker

--- src/test/pass_checker/PasswordChecker.test.ts
+++ src/test/pass_checker/PasswordChecker.test.ts
@@ -7,7 +7,33 @@ describe('PasswordChecker test suite', () => {
     sut = new PasswordChecker()
   })
 
-  it('should do nothing for the moment', () => {
-    sut.checkPassword()
+  it('Password with less than 8 chars is invalid', () => {
+    const actual = sut.checkPassword('1234567')
+    expect(actual).toBe(false)
+  })
+
+  it('Password with more than 8 chars is ok', () => {
+    const actual = sut.checkPassword('12345678Aa')
+    expect(actual).toBe(true)
+  })
+
+  it('Password with no upper case letter is invalid', () => {
+    const actual = sut.checkPassword('1234abcd')
+    expect(actual).toBe(false)
+  })
+
+  it('Password with upper case letter is valid', () => {
+    const actual = sut.checkPassword('1234abcdA')
+    expect(actual).toBe(true)
+  })
+
+  it('password with no lower case letter is invalid', () => {
+    const actual = sut.checkPassword('1234ABCD')
+    expect(actual).toBe(false)
+  })
+
+  it('Password with lower case letter is valid', () => {
+    const actual = sut.checkPassword('1234ABCDa')
+    expect(actual).toBe(true)
   })
 })
