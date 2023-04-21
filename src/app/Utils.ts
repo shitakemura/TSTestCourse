f32968a435f6cb216f0b341934dbd23534f27431
shitakemura <shitakemura@gmail.com>
2023-04-21 13:37:24 +0900

    jest assertions and mathers

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,21 @@
 export function toUpperCase(arg: string) {
   return arg.toUpperCase()
 }
+
+export type stringInfo = {
+  lowerCase: string
+  upperString: string
+  characters: string[]
+  length: number
+  extraInfo: Object | undefined
+}
+
+export function getStringInfo(arg: string): stringInfo {
+  return {
+    lowerCase: arg.toLowerCase(),
+    upperString: arg.toUpperCase(),
+    characters: Array.from(arg),
+    length: arg.length,
+    extraInfo: {},
+  }
+}
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,33 @@
-import { toUpperCase } from '../app/Utils'
+import { getStringInfo, toUpperCase } from '../app/Utils'
 
 describe('Utils test suite', () => {
-  test('should return uppercase', () => {
-    const result = toUpperCase('abc')
-    expect(result).toBe('ABC')
+  it('should return uppercase', () => {
+    const sut = toUpperCase
+    const expected = 'ABC'
+
+    const actual = sut('abc')
+
+    expect(actual).toBe(expected)
+  })
+
+  it('should return info for valid string', () => {
+    const actual = getStringInfo('My-String')
+
+    expect(actual.lowerCase).toBe('my-string')
+    expect(actual.extraInfo).toEqual({})
+
+    expect(actual.length).toBe(9)
+    expect(actual.characters).toHaveLength(9)
+
+    expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g'])
+    expect(actual.characters).toContain('M')
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-'])
+    )
+
+    expect(actual.extraInfo).not.toBe(undefined)
+    expect(actual.extraInfo).not.toBeUndefined()
+    expect(actual.extraInfo).toBeDefined()
+    expect(actual.extraInfo).toBeTruthy()
   })
 })
