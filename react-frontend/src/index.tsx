ad740eba11d934c13d4e0e7252ed2880064e8a4f
shitakemura <shitakemura@gmail.com>
2023-04-26 09:51:05 +0900

    setup project

---
+++ react-frontend/src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import LoginComponent from './LoginComponent'
+import LoginService from './services/LoginService'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const loginService = new LoginService()
+const setToken = (token: string) => {
+  console.log(`received the token ${token}`)
+}
+
+root.render(<LoginComponent loginService={loginService} setToken={setToken} />)
